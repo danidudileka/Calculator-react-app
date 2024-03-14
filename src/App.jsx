b4114554220c50d87aa457679d3737875c22d787
danidudileka <danidudilekaperera@gmail.com>
2024-03-14 12:59:44 +0530

    html and css implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
+      <div class="container">
+        <form>
+          <div class="screen">
+            <p>123456</p>
+          </div>
+          <input type="button" value="(" />
+          <input type="button" value=")" />
+          <input type="button" value="AC" />
+          <input type="button" value="DE" />
 
-    </>
-  )
+          <input type="button" value="+" />
+          <input type="button" value="-" />
+          <input type="button" value="*" />
+          <input type="button" value="/" />
+
+          <input type="button" value="7" />
+          <input type="button" value="8" />
+          <input type="button" value="9" />
+          <input type="button" value="**" />
+
+          <input type="button" value="4" />
+          <input type="button" value="5" />
+          <input type="button" value="6" />
+          <input type="button" value="=" class="span-row-3" />
+
+          <input type="button" value="1" />
+          <input type="button" value="2" />
+          <input type="button" value="3" />
+
+          <input type="button" value="0" />
+          <input type="button" value="00" />
+          <input type="button" value="." />
+        </form>
+      </div>
+  );
 }
 
-export default App
+export default App;
